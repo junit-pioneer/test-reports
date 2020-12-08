67d95a011333e1553b1afdcb0082b81642b514ee
Matthias Bünger <hardcore_army@web.de>
2020-12-08 17:55:05 +0100

    Initial setup (#1)
    This PR sets up the Gradle project based on the extension pack. Some parts are still to be done,
    e.g. moditect, module-info.java etc.

---
+++ src/main/module/module-info.java
@@ -0,0 +1,6 @@
+module test-reports {
+	// Must be set up after initial source code
+	requires org.junit.jupiter.api;
+	requires org.junit.jupiter.engine;
+	requires org.junit-pioneer.junit-pioneer;
+}
