748c4cef37b48f66aa17e8a18491acd380dfe3f1
Matthias Bünger <Bukama@users.noreply.github.com>
2022-04-26 18:46:17 +0200

    Beginning to update dependencies (#41 / #42)
    The project uses several outdated dependencies including Pioneer itself but also versions of Log4J containing CVEs (Log4Shell)
    
    closes: #41
    PR: #42

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,98 +1,79 @@
 plugins {
-	java
-	jacoco
-	checkstyle
-	`maven-publish`
-	id("com.diffplug.spotless") version "5.10.2"
-	id("org.shipkit.java") version "2.3.5"
-	id("at.zierler.yamlvalidator") version "1.5.0"
-	//id("org.sonarqube") version "3.0"
-	id("org.moditect.gradleplugin") version "1.0.0-rc3"
+    java
+    jacoco
+    checkstyle
+    `maven-publish`
+    id("com.diffplug.spotless") version "6.4.2"
+    id("org.shipkit.java") version "2.3.5"
+    id("at.zierler.yamlvalidator") version "1.5.0"
+    //id("org.sonarqube") version "3.0"
 }
 
 plugins.withType<JavaPlugin>().configureEach {
-	configure<JavaPluginExtension> {
-		modularity.inferModulePath.set(true)
-	}
+    configure<JavaPluginExtension> {
+        modularity.inferModulePath.set(true)
+    }
 }
 
 group = "org.junit-pioneer"
 description = "Additional test reports for JUnit Jupiter and its extensions"
 
-val modularBuild = findProperty("modularBuild") != null;
 
 java {
-	if(modularBuild) {
-		sourceCompatibility = JavaVersion.VERSION_11
-	} else {
-		sourceCompatibility = JavaVersion.VERSION_1_8
-	}
-
+    sourceCompatibility = JavaVersion.VERSION_1_8
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
-val junitVersion : String by project
+val junitVersion: String by project
 
 dependencies {
-	implementation(platform("org.junit:junit-bom:$junitVersion"))
+    implementation(platform("org.junit:junit-bom:$junitVersion"))
 
-	implementation(group = "org.junit-pioneer", name = "junit-pioneer", version = "1.3.8")
-	implementation(group = "org.junit.jupiter", name = "junit-jupiter-engine")
+    implementation(group = "org.junit-pioneer", name = "junit-pioneer", version = "1.7.0")
+    implementation(group = "org.junit.jupiter", name = "junit-jupiter-engine")
 
-	implementation(group = "com.opencsv", name = "opencsv", version = "5.3")
-	implementation(group = "javax.xml.bind", name = "jaxb-api", version = "2.3.1")
-	implementation(group = "com.sun.xml.bind", name = "jaxb-impl", version = "2.3.3")
+    implementation(group = "com.opencsv", name = "opencsv", version = "5.3")
+    implementation(group = "javax.xml.bind", name = "jaxb-api", version = "2.3.1")
+    implementation(group = "com.sun.xml.bind", name = "jaxb-impl", version = "2.3.3")
 
-	testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api")
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter-api")
 
-	testImplementation(group = "org.assertj", name = "assertj-core", version = "3.19.0")
-	testImplementation(group = "org.mockito", name = "mockito-core", version = "3.7.7")
-	testImplementation(group = "com.google.jimfs", name = "jimfs", version = "1.2")
+    testImplementation(group = "org.assertj", name = "assertj-core", version = "3.22.0")
+    testImplementation(group = "org.mockito", name = "mockito-core", version = "4.5.1")
+    testImplementation(group = "com.google.jimfs", name = "jimfs", version = "1.2")
 
-	testRuntimeOnly(group = "org.apache.logging.log4j", name = "log4j-core", version = "2.14.0")
-	testRuntimeOnly(group = "org.apache.logging.log4j", name = "log4j-jul", version = "2.14.0")
+    testRuntimeOnly(group = "org.apache.logging.log4j", name = "log4j-core", version = "2.17.1")
+    testRuntimeOnly(group = "org.apache.logging.log4j", name = "log4j-jul", version = "2.17.1")
 }
 
 spotless {
-	val headerFile = file(".infra/spotless/eclipse-public-license-2.0.java")
+    val headerFile = file(".infra/spotless/eclipse-public-license-2.0.java")
 
-	java {
-		licenseHeaderFile(headerFile, "(package|import) ")
-		importOrderFile(file(".infra/eclipse/junit-eclipse.importorder"))
-		eclipse().configFile(".infra/eclipse/junit-eclipse-formatter-settings.xml")
+    java {
+        licenseHeaderFile(headerFile, "(package|import) ")
+        importOrderFile(file(".infra/eclipse/junit-eclipse.importorder"))
+        eclipse().configFile(".infra/eclipse/junit-eclipse-formatter-settings.xml")
 
-		trimTrailingWhitespace()
-		endWithNewline()
-		removeUnusedImports()
-	}
-
-	format("groovy") {
-		target("**/*.groovy")
-		indentWithTabs()
-		trimTrailingWhitespace()
-		endWithNewline()
-		licenseHeaderFile(headerFile, "package ")
-
-		replaceRegex("class-level Javadoc indentation fix", """^\*""", " *")
-		replaceRegex("nested Javadoc indentation fix", "\t\\*", "\t *")
-	}
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
 
 checkstyle {
-	toolVersion = "7.8.2"
-	configDirectory.set(rootProject.file(".infra/checkstyle"))
+    toolVersion = "7.8.2"
+    configDirectory.set(rootProject.file(".infra/checkstyle"))
 }
 
 yamlValidator {
-	searchPaths = listOf("docs")
-	isSearchRecursive = true
+    searchPaths = listOf("docs")
+    isSearchRecursive = true
 }
 
 jacoco {
-	toolVersion = "0.8.6"
+    toolVersion = "0.8.8"
 }
 
 // Until project is created
@@ -107,71 +88,70 @@ jacoco {
 //	}
 //}
 
-//moditect {
-//	addMainModuleInfo {
-//		version = project.version
-//		overwriteExistingFiles.set(true)
-//		module {
-//			moduleInfoFile = rootProject.file("src/main/module/module-info.java")
-//		}
-//	}
-//}
 
 tasks {
 
-	sourceSets {
-		main {
-			if (modularBuild)
-				java.srcDir("src/main/module")
-		}
-	}
+    sourceSets {
+        main {
 
-	compileJava {
-		options.encoding = "UTF-8"
-	}
+        }
+    }
 
-	compileTestJava {
-		options.encoding = "UTF-8"
-	}
+    compileJava {
+        options.encoding = "UTF-8"
+        options.compilerArgs.add("-Werror")
+        options.compilerArgs.add("-Xlint:all")
+    }
 
-	test {
-		testLogging {
-			setExceptionFormat("full")
-		}
-		useJUnitPlatform()
-		filter {
-			includeTestsMatching("*Tests")
-		}
-		systemProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager")
-	}
+    compileTestJava {
+        options.encoding = "UTF-8"
+        options.compilerArgs.add("-Werror")
+        options.compilerArgs.add("-Xlint:all")
+    }
 
-	javadoc {
-		// of all the javadoc checks (accessibility, html, missing, reference, syntax; see
-		// https://docs.oracle.com/javase/8/docs/technotes/tools/unix/javadoc.html#BEJEFABE)
-		// disable the warning for missing comments and tags because they spam the output
-		// (it does often not make sense to comment every tag; e.g. the @return tag on annotations)
-		(options as CoreJavadocOptions).addStringOption("Xdoclint:accessibility,html,syntax,reference", "-quiet")
-		options.encoding = "UTF-8"
-		shouldRunAfter(test)
-	}
+    test {
+        testLogging {
+            setExceptionFormat("full")
+        }
+        useJUnitPlatform()
+        filter {
+            includeTestsMatching("*Tests")
+        }
+        systemProperty("java.util.logging.manager", "org.apache.logging.log4j.jul.LogManager")
 
-	jacocoTestReport {
-		reports {
-			xml.isEnabled = true
-			xml.destination = file("${buildDir}/reports/jacoco/report.xml")
-		}
-	}
+        jvmArgs(
+            "-XX:+IgnoreUnrecognizedVMOptions",
+            "--add-opens=java.base/java.util=ALL-UNNAMED"
+        )
+    }
 
-	check {
-		// to find Javadoc errors early, let "javadoc" task run during "check"
-		dependsOn(javadoc, validateYaml)
-	}
+    javadoc {
+        // of all the javadoc checks (accessibility, html, missing, reference, syntax; see
+        // https://docs.oracle.com/javase/8/docs/technotes/tools/unix/javadoc.html#BEJEFABE)
+        // disable the warning for missing comments and tags because they spam the output
+        // (it does often not make sense to comment every tag; e.g. the @return tag on annotations)
+        (options as CoreJavadocOptions).addStringOption("Xdoclint:accessibility,html,syntax,reference", "-quiet")
+        options.encoding = "UTF-8"
+        shouldRunAfter(test)
+    }
 
-	withType<Jar>().configureEach {
-		from(projectDir) {
-			include("LICENSE.md")
-			into("META-INF")
-		}
-	}
+    jacocoTestReport {
+        reports {
+            xml.isEnabled = true
+            xml.destination = file("${buildDir}/reports/jacoco/report.xml")
+        }
+    }
+
+    check {
+        // to find Javadoc errors early, let "javadoc" task run during "check"
+        dependsOn(javadoc, validateYaml)
+    }
+
+    withType<Jar>().configureEach {
+        from(projectDir) {
+            include("LICENSE.md")
+            into("META-INF")
+        }
+    }
 
 }
